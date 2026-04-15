3e8fa7cbfcc07425f1578e6a4b336b027dfb1c7f
Dobó Heléna Lilla <85240382+LillaDobo@users.noreply.github.com>
2026-04-16 01:21:43 +0200

    Telepítési scriptek
    Win11, windows server, linus gép telepítése

---
+++ linux.ps1
@@ -0,0 +1,74 @@
+$username = "azureuser"
+$Password = Read-Host "Enter VM password" -AsSecureString
+$cred = New-Object System.Management.Automation.PSCredential($username, $Password)
+
+$location = "switzerlandnorth"
+$rg = "zh"
+$VMName = "W1C4GQ"
+$size = "Standard_B2s"
+$publisher = "Debian"
+$offer     = "debian-12"
+$sku       = "12-gen2"  
+$version   = "latest"
+
+$VnetName = "MyNet"
+$NICName = "MyNIC"
+$SubnetName = "MySubnet"
+$SubnetCidr = "10.0.0.0/24"
+$VnetCidr = "10.0.0.0/16"
+$NSGName    = "MyNSG"
+$PipName = "MyPIP"
+
+
+
+new-azResourceGroup -Location $location -Name $rg
+
+$pip = New-AzPublicIpAddress -Name $PipName -ResourceGroupName $rg -Location $location `
+       -AllocationMethod Static -Sku Standard
+
+$vnet = New-AzVirtualNetwork -Name $VnetName -ResourceGroupName $rg -Location $location `
+        -AddressPrefix $VnetCidr
+$vnet = Add-AzVirtualNetworkSubnetConfig -Name $SubnetName -VirtualNetwork $vnet -AddressPrefix $SubnetCidr
+$vnet = Set-AzVirtualNetwork -VirtualNetwork $vnet
+$vnet   = Get-AzVirtualNetwork -Name $VnetName -ResourceGroupName $rg
+$subnet = Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $SubnetName
+$subnetId = $subnet.Id
+
+$nsg = New-AzNetworkSecurityGroup -Name $NSGName -ResourceGroupName $rg -Location $location
+$Params = @{
+  Name                     = 'allowSSH'
+  NetworkSecurityGroup     = $nsg
+  Protocol                 = 'TCP'
+  Direction                = 'Inbound'
+  Priority                 = 200
+  SourceAddressPrefix      = '*'
+  SourcePortRange          = '*'
+  DestinationAddressPrefix = '*'
+  DestinationPortRange     = 22
+  Access                   = 'Allow'
+}
+Add-AzNetworkSecurityRuleConfig @Params | Set-AzNetworkSecurityGroup | Out-Null
+
+$nic = New-AzNetworkInterface -Name $NICName -ResourceGroupName $rg -Location $location  -SubnetId $subnetId -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+
+$vm = New-AzVMConfig -VMName $vmName -VMSize $size |
+  Set-AzVMOperatingSystem -Linux -ComputerName $vmName -Credential $cred |
+  Set-AzVMSourceImage -PublisherName $publisher -Offer $offer -Skus $sku -Version $version |
+  Set-AzVMOSDisk -CreateOption FromImage -Name "$vmName-osdisk" -StorageAccountType Standard_LRS |  Add-AzVMNetworkInterface -Id $nic.Id
+
+New-AzVM -ResourceGroupName $rg -Location $location -VM $vm -Verbose
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
